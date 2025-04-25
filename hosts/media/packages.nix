dd95d03636b893d3d412e6df00587a0f3510d2b3
coolguy1842 <github.com.freely529@passfwd.com>
2025-04-25 15:00:01 +1000

    remove packages

--- hosts/media/packages.nix
+++ hosts/media/packages.nix
@@ -1,6 +1,3 @@
-{ pkgs, ... }: {
-    environment.systemPackages = with pkgs; [
-        vscode
-        firefox
-    ];
+{ inputs, pkgs, ... }: {
+    environment.systemPackages = with pkgs; [];
 }
